ce41e704fa93dfb40afa04c4a8c41a2930606058
Under <jorgeolmo.I@gmail.com>
2022-11-15 14:55:30 +0100

    update option size

--- src/components/Options.vue
+++ src/components/Options.vue
@@ -4,7 +4,7 @@
             <div class="fixed inset-0 bg-gray-400 bg-opacity-10 transition-opacity"></div>
             <div class="fixed inset-0 z-10 overflow-y-auto">
                 <div class="flex min-h-full justify-center p-4 items-center">
-                    <div class="relative transform overflow-hidden rounded-lg bg-img shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                    <div class="relative transform overflow-hidden rounded-lg bg-img shadow-xl transition-all pr-20 pb-5 pt-5 pl-10">
                         <div class="px-4 py-3 pt-16 pl-10 pr-10 grid grid-cols-4 gap-4">
                             <div class="text-3xl" v-if="idiom == 'English'" >
                                 Options
@@ -105,7 +105,7 @@
                                     <input v-model="numObj" type="number" id="item" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
                                 </div>
                             </div>
-                        </div>
+                        </div>  
                         <div class="px-4 py-3 pb-10 sm:flex sm:flex-row-reverse sm:px-6"> <!-- Botones -->
                             <!-- <button type="button" class="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm">Deactivate</button> -->
                             <button v-if="idiom == 'English'" type="button" class="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-blue-400 px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm" v-on:click="accept" >Accept</button>
